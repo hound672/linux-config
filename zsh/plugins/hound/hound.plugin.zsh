cdd058bb34ab30c6ede3d8efa00a3aea1884068e
Vasilii Bliznetsov <hound672@gmail.com>
2024-01-10 23:18:08 +0300

    [zsh] Add some validation in plugin

--- zsh/plugins/hound/hound.plugin.zsh
+++ zsh/plugins/hound/hound.plugin.zsh
@@ -24,6 +24,12 @@ function kc_add_config {
     return 1
   fi
 
+  if [ ! -f $config_to_add ]
+  then
+      echo "File "${(%):-"%B${config_to_add}%b does not exist."}
+      return 1
+  fi
+
   local current_config="${KUBECONFIG:-~/.kube/config}"
 
   if [[ $current_config == *${config_to_add}* ]]
