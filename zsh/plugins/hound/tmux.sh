57e7c36df3c2bb47f4373f2cdf1ac1bf5b74f37e
Vasilii Bliznetsov <hound672@gmail.com>
2024-02-13 10:25:55 +0300

    [tmux| Add tmux_resurrect_backup command

--- zsh/plugins/hound/tmux.sh
+++ zsh/plugins/hound/tmux.sh
@@ -1,7 +1,7 @@
 # ===== TMUX commands =====
 
 # clear old tmux-resurrect sessions
-function tmux_clear_resurrect {
+function tmux_resurrect_clear {
   local current_dir=$PWD
 
   local resurrect_path=$(tmux show -g @resurrect-dir | awk '{print $2}')
@@ -14,6 +14,28 @@ function tmux_clear_resurrect {
     echo "Remove file: "${(%):-"%B${session_file_name}%b"}
     rm -f $session_file_name
   done
+
+  cd $current_dir
+}
+
+function tmux_resurrect_backup {
+  local backup_path="${1:-}"
+
+  if [ -z $backup_path ]
+    then
+      echo "Backup path is not specified"
+      return 1
+    fi
+
+  local current_dir=$PWD
+
+  local resurrect_path=$(tmux show -g @resurrect-dir | awk '{print $2}')
+  cd $resurrect_path
+
+  local last=$(readlink last)
+
+  cp $last $backup_path
+  echo "Backup file: "${(%):-"%B${last}%b to %B${backup_path}%b"}
 
   cd $current_dir
 }
