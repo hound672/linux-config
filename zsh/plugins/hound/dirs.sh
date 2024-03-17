a3f5b67ceba44a3c2be3fb0b56f01d8348c3927e
Vasilii Bliznetsov <hound672@gmail.com>
2024-03-17 23:24:18 +0300

    [zsh] Add rmcd command

--- zsh/plugins/hound/dirs.sh
+++ zsh/plugins/hound/dirs.sh
@@ -1,6 +1,15 @@
 # ===== DIRS commands =====
 
+# create dir and enter to it
 function mdcd() {
   mkdir "$1"
   cd "$1"
+}
+
+# remove current dir
+function rmcd() {
+    local current=${PWD}
+    cd ..
+    rm -r ${current}
+    echo ${(%):-"%B${current:a}%b deleted."}
 }
