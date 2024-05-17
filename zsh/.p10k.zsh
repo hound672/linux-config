fb845a8899df08ec9fd0ba80b1ecfece33b7c3e1
Vasilii Bliznetsov <hound672@gmail.com>
2024-05-17 14:33:59 +0300

    [zsh] Add time and move status to newline

--- zsh/.p10k.zsh
+++ zsh/.p10k.zsh
@@ -44,7 +44,6 @@
   # automatically hidden when the input line reaches it. Right prompt above the
   # last prompt line gets hidden if it would overlap with left prompt.
   typeset -g POWERLEVEL9K_RIGHT_PROMPT_ELEMENTS=(
-    status                  # exit code of the last command
     command_execution_time  # duration of the last command
     background_jobs         # presence of background jobs
     direnv                  # direnv status (https://direnv.net/)
@@ -104,7 +103,10 @@
     taskwarrior             # taskwarrior task count (https://taskwarrior.org/)
     per_directory_history   # Oh My Zsh per-directory-history local/global indicator
     # cpu_arch              # CPU architecture
-    # time                  # current time
+    time                    # current time
+    # =========================[ Line #2 ]=========================
+    newline
+    status                  # exit code of the last command
     # ip                    # ip address and bandwidth usage for a specified network interface
     # public_ip             # public IP address
     # proxy                 # system-wide http/https/ftp proxy
